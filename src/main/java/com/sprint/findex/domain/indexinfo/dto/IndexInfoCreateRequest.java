943c0d0e6110c8498703f3c10a8bf954baf40cbd
김사연 <4yonnzzang@gmail.com>
2026-03-12 15:19:31 +0900

    feat: DTO 관련 파일 추가

--- src/main/java/com/sprint/findex/domain/indexinfo/dto/IndexInfoCreateRequest.java
+++ src/main/java/com/sprint/findex/domain/indexinfo/dto/IndexInfoCreateRequest.java
@@ -3,15 +3,15 @@ package com.sprint.findex.domain.indexinfo.dto;
 import com.sprint.findex.common.type.SourceType;
 import java.math.BigDecimal;
 
-//새로운 지수 정보를 등록할 때 클라이언트로 전달받는 DTO
+/**새로운 지수 정보를 등록할 때 사용하는 DTO */
 public record IndexInfoCreateRequest(
-  String idxCsf,
-  String idxNm,
-  Integer epyItmsCnt,
-  BigDecimal basIdx,
-  SourceType sourceType, //사용자가 직접 등록하는지, Open API를 통하는지 구분
+  String indexClassificationName,
+  String indexName,
+  Integer employedItemsCount,
+  String basePointInTime,
+  BigDecimal baseIndex,
+  SourceType sourceType,
   Boolean favorite
-){}
+) {}
 
 
-//엔티티 소스타입,즐겨찾기 순서 바꾸기
--- src/main/java/com/sprint/findex/domain/indexinfo/dto/IndexInfoUpdateRequest.java
+++ src/main/java/com/sprint/findex/domain/indexinfo/dto/IndexInfoUpdateRequest.java
@@ -1,6 +1,11 @@
 package com.sprint.findex.domain.indexinfo.dto;
 
-//기존 지수 정보의 세부 수치를 수정할 때 사용하는 DTO
+import java.math.BigDecimal;
+
+/**기존 지수 정보의 세부 수치를 수정할 때 사용하는 DTO */
 public record IndexInfoUpdateRequest(
-    Integer epu
-){}
+    Integer employedItemsCount,
+    String basePointInTime,
+    BigDecimal baseIndex,
+    Boolean favorite
+) {}
