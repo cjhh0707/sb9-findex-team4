90b44fff75aea826e27dcb1fb3dddba4b8458f54
cjhh0707 <119827971+cjhh0707@users.noreply.github.com>
2026-03-13 09:15:11 +0900

    Revert "Feature/index data"

--- src/main/java/com/sprint/findex/domain/indexdata/repository/IndexDataRepository.java
+++
@@ -1,8 +0,0 @@
-package com.sprint.findex.domain.indexdata.repository;
-
-import com.sprint.findex.domain.indexdata.entity.IndexData;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IndexDataRepository extends JpaRepository<IndexData, Long> {
-
-}
