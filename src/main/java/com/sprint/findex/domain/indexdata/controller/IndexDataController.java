cad825484dcc8b07815e096ecea044175be3745e
seo hyeon ha <hyeonha1103@gmail.com>
2026-03-17 09:38:19 +0900

    feat: 스웨거 명세 준수를 위한 API 인터페이스 정의 및 적용

--- src/main/java/com/sprint/findex/domain/indexdata/controller/IndexDataController.java
+++ src/main/java/com/sprint/findex/domain/indexdata/controller/IndexDataController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.*;
 
 
 @RestController
-@RequestMapping("/api/v1/index-data")
+@RequestMapping("/api/index-data")
 @RequiredArgsConstructor
 public class IndexDataController {
 
